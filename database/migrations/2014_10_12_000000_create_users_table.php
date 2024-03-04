8d93343b74d9b3b5e2926aaf8729ca3729893fa6
Hamaad Kaleem <46200983+Hamaad616@users.noreply.github.com>
2024-03-05 00:39:26 +0500

    index removed

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,6 @@ return new class extends Migration
             $table->string('name');
             $table->string('phone');
             $table->text('address');
-            $table->index('cnic');
             $table->boolean('verified');
             $table->timestamps();
         });
