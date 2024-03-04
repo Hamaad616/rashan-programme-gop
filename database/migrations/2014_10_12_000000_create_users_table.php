9cdedc48a18795428c6d7154afa1c969ca6bc9b3
Hamaad Kaleem <46200983+Hamaad616@users.noreply.github.com>
2024-03-05 00:36:42 +0500

    index removed

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('cnic')->unique();
-            $table->index('cnic');
             $table->string('name');
             $table->string('phone');
             $table->text('address');
